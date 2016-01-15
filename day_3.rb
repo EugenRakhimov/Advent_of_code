2ccb5abc913e9d7d0fb095bfa78e2a85d508786c
EugenRakhimov <gin3002@gmail.com>
2016-01-15 19:49:09 +1300

    day 3 part 2

--- day_3.rb
+++ day_3.rb
@@ -42,15 +42,38 @@ class VisitingHouse < ReadInput
     return Coordinate.new(coordinate.x+1, coordinate.y) if ch == '>'
     return Coordinate.new(coordinate.x-1, coordinate.y) if ch == '<' 
   end
-
-  def parse_line(line)
+  def visiting_house(line)
     current_coordinate = Coordinate.new(0,0)
     homes = [current_coordinate]
     line.chars.each do |ch|
       current_coordinate = next_coordinate(ch, current_coordinate)
       homes << current_coordinate if !homes.include?(current_coordinate)
+      puts current_coordinate.x.to_s+" "+ current_coordinate.y.to_s
     end
-    homes.count
+    return homes
+  end
+
+
+  def parse_line(line)
+    count = 0 
+    line_santa = ""
+    line_robo = ""
+    line.chars.each do |ch|
+      if (count%2 == 1)
+        line_robo << ch
+      else
+        line_santa << ch
+      end
+      count = count+1
+    end
+    puts line_santa
+    puts line_robo
+    homes_santa = visiting_house(line_santa)
+    homes_robo = visiting_house(line_robo)
+    homes_robo.each do |coordinate|
+      homes_santa << coordinate if !homes_santa.include? coordinate
+    end
+    return homes_santa.count
   end
 end
 vh = VisitingHouse.new("day_3.txt")
