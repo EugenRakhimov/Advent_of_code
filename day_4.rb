2854220923f063a794dc61e23506bd2f2e95569a
EugenRakhimov <gin3002@gmail.com>
2016-01-15 20:08:43 +1300

    day 4 part 1

---
+++ day_4.rb
@@ -0,0 +1,24 @@
+require 'digest'
+def calculate_hash(number)
+  md5 = Digest::MD5.new
+  md5 << 'bgvyzdsv'
+  md5 << number.to_s
+  md5.hexdigest 
+end
+def check_hash(hash)
+   if (hash[0,5] == "00000")
+      return true
+    else
+      return false
+   end
+end
+def find_a_number
+  number = 1
+  while(!check_hash(calculate_hash(number)))
+
+    number = number+1
+    
+  end
+  number
+end
+puts find_a_number
