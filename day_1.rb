bde14e3ce0be71dfc89879d72fc217bf924219cb
EugenRakhimov <gin3002@gmail.com>
2016-01-15 17:21:29 +1300

    day 1 part 2

--- day_1.rb
+++ day_1.rb
@@ -9,11 +9,24 @@ def parse_line(line)
   end
   result
 end
+def find_basement(line)
+   result = 0
+   count = 0
+   line.chars.each do |ch|
+    result = result + check(ch)
+    count = count+1
+      if (result < 0) 
+        return count
+      end
+    end
+
+end
 
 file = File.new("day_1.txt", "r")
 result = 0
 while (line = file.gets)
     result += parse_line(line)
+    puts find_basement(line)
 end
 file.close
 puts result
