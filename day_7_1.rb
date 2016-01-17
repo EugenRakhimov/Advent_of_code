c976a4f993a0b9efce868043f2309b5cfdbc691d
EugenRakhimov <gin3002@gmail.com>
2016-01-17 22:31:11 +1300

    day 7 part 1

--- day_7_1.rb
+++ day_7_1.rb
@@ -23,7 +23,9 @@ class Wire_Directions_Reader
   end
   def parse_direction(line)
     arr = line.split(' ')      
-    return Direction.new(arr[2],"line","line",arr[0].to_i,"SET")  if (arr[1] == "->")
+    return Direction.new(arr[2],"line","line",arr[0].to_i,"SET")  if ((arr[1] == "->")&&(arr[0].to_i.to_s == arr[0]))
+    return Direction.new(arr[2],arr[0],"line",-1,"TRF")  if (arr[1] == "->")
+   
     return Direction.new(arr[3], arr[1],"line",-1,"NOT")  if (arr[0] == "NOT")
     return Direction.new(arr[4], arr[0],arr[2],-1,arr[1]) 
   end
@@ -46,7 +48,7 @@ class WireControler
   def print_directions
     ready_directions = get_ready_directions
     ready_directions.each do |direction|
-      p direction
+      puts direction
     end
   end
   def do_not_command(direction)
@@ -54,7 +56,8 @@ class WireControler
   end  
   def execute_direction(direction)
     first_signal = get_signal(direction.first_wire)
-    return direction.signal = (~first_signal) if direction.operation == "NOT"
+    return direction.signal = (~first_signal) if (direction.operation == "NOT")
+    return direction.signal = first_signal if (direction.operation == "TRF")
     second_signal = get_signal(direction.second_wire)       
     return direction.signal = (first_signal&second_signal) if direction.operation == "AND"
     return direction.signal = (first_signal|second_signal) if direction.operation == "OR"
@@ -65,39 +68,42 @@ class WireControler
   def do_directions(ready_directions)
     ready_directions.each do |direction|
       execute_direction(direction)
+      # p direction
     end
   end
   def find_a_signal   
-    while (get_signal("a") < 0) do
+    while (get_signal("a") == -1) do
       ready_directions = get_ready_directions
       do_directions(ready_directions)
+      # p @directions
     end
     get_signal("a")    
   end
   def get_signal(name)
     return name.to_i if name.to_i.to_s == name
-    direction = @directions.select{|d| d.name == name}[0]
-    p direction
+    # puts name
+    direction = @directions.select{|d| d.name == name}[0]    
     return direction.signal.to_i 
   end
   def ready(direction)
     # exclude already calculated
-    return false if direction.signal > -1
+    return false if direction.signal != -1
     
     first_signal = get_signal(direction.first_wire) if (direction.first_wire!="line")
     
     return false if (first_signal == -1)
-    return true if ((first_signal >-1)&& (direction.operation == "NOT"))
+    return true if ((first_signal != -1)&& ((direction.operation == "NOT")||(direction.operation == "TRF")))
     second_signal = get_signal(direction.second_wire)
     
     return false if (second_signal == -1)    
-    return true if  (second_signal > -1)
+    return true if  (second_signal != -1)
     return false
   end
   def get_ready_directions
     ready_directions = []
     @directions.each do |direction|
       ready_directions << direction if ready(direction)
+      p direction
     end
     ready_directions
   end
@@ -105,4 +111,5 @@ class WireControler
 end
 wireController = WireControler.new
 wireController.print_directions
+# p wireController.get_signal("a")
 p wireController.find_a_signal
